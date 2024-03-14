a350c3f4d112ea09b31ffa3d6bf4cff886aba046
Aricoria10 <empresskitsune10@gmail.com>
2024-03-14 05:53:17 -0400

    added resume outline

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,23 @@
 import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <div>
-          <Link className="text-light" to="/">
-            <h1 className="m-0">D&D-GO</h1>
-          </Link>
-        </div>
         <div>
           <>
-            <Link className="btn btn-lg btn-info m-2" to="/about">
+            <Link to="/about">
               About me
             </Link>
-            <Link className="btn btn-lg btn-info m-2" to="/portfolio">
+            <Link to="/portfolio">
               Portfolio
             </Link>
-            <Link className="btn btn-lg btn-info m-2" to="/contact">
+            <Link to="/contact">
               Contact
             </Link>
-            <Link className="btn btn-lg btn-light m-2" to="/resume">
+            <Link to="/resume">
               Resume
             </Link>
           </>
         </div>
-      </div>
-    </div>
   );
 };
 export default Navbar;
